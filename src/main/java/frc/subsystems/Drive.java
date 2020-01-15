f4d2145724dd920fc92e7d409b7365ac043e2730
Nick Farley <nick-1154@hotmail.com>
2020-01-14 18:42:24 -0600

    Created Dive Class
    (Max did this)

---
+++ src/main/java/frc/subsystems/Drive.java
@@ -0,0 +1,52 @@
+package frc.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Drive {
+
+    private TalonSRX backLeft;
+    private TalonSRX backRight;
+    private TalonSRX frontLeft;
+    private TalonSRX frontRight;
+
+    public Drive(int backleftID, int backrightID, int frontleftID, int frontrightID) {
+        backLeft = new TalonSRX(backleftID);
+        backRight = new TalonSRX(backrightID);
+        frontLeft = new TalonSRX(frontleftID);
+        frontRight = new TalonSRX(frontrightID);
+
+        frontRight.setInverted(true);
+        backRight.setInverted(true);
+
+    }
+
+    /**
+     * This will run the left side wheels of the robot and the right side wheels of
+     * the robot at the given speeds
+     */
+    public void tankDrive(double left, double right) {
+        runBackLeft(left);
+        runFrontLeft(left);
+        runBackRight(right);
+        runFrontRight(right);
+    }
+
+    /** Run the back left moter at the given speed */
+    public void runBackLeft(double speed) {
+        backLeft.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void runBackRight(double speed) {
+        backRight.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void runFrontLeft(double speed) {
+        frontLeft.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void runFrontRight(double speed) {
+        frontRight.set(ControlMode.PercentOutput, speed);
+    }
+
+}
