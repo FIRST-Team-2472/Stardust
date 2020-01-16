208abaa7b676d29fd84a94884b6e53e7bfbe87f5
Nick Farley <nick-1154@hotmail.com>
2020-01-16 16:12:24 -0600

    Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Constants {
+    public static int motorBL = 0;
+	public static int motorBR = 1;
+	public static int motorFL = 2;
+	public static int motorFR = 3;
+	public static int jstickL = 0;//left flight stick
+    public static int jstickR = 0;//right flight stick
+}
