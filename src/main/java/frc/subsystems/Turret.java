f2b677d0aec29c0da03658edfd605ce60d8bffe1
Nick Farley <nick-1154@hotmail.com>
2020-01-18 11:11:48 -0600

    Turret

---
+++ src/main/java/frc/subsystems/Turret.java
@@ -0,0 +1,18 @@
+package frc.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Turret {
+    
+    private TalonSRX turret;
+    
+    public Turret(int turretID) {
+        turret = new TalonSRX(turretID);
+    }
+
+    public void runTurret(double speed) {
+        turret.set(ControlMode.PercentOutput,speed);
+    }
+
+}
