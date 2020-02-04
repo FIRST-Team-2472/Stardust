8ddba8c3045dbce300ea418c1f1b449e20d82892
Nick Farley <nick-1154@hotmail.com>
2020-02-04 15:39:50 -0600

    Revert "yay"
    This reverts commit 4c37f1fb2a66e78d4c78b0cf53d384e6403802d1.

--- src/main/java/frc/robot/Limelight.java
+++ src/main/java/frc/robot/Limelight.java
@@ -1,29 +1,15 @@
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight {
-
-    public Limelight() {
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-        NetworkTableEntry tx = table.getEntry("tx");
-        NetworkTableEntry ty = table.getEntry("ty");
-        NetworkTableEntry ta = table.getEntry("ta");
-        table.getEntry("ledMode").setNumber(0);
-        table.getEntry("camMode").setNumber(0);
-        table.getEntry("pipeline").setNumber(0);
-
-    }
-
     
 
     public boolean isTargetSpoted() {
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0) == 1.0;
     }
     public double targetXAngleFromCenter() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(-999);
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
     }
     public double distanceCM() {
         double targetAngle = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
