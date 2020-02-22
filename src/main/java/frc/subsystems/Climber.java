03c379e234f9242b495576dfb9e10514bbaaf3ed
Nick Farley <nick-1154@hotmail.com>
2020-02-22 12:56:59 -0600

    shooting/climbing patch

--- src/main/java/frc/subsystems/Climber.java
+++ src/main/java/frc/subsystems/Climber.java
@@ -3,44 +3,26 @@ package frc.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+/** System to make the robot climb */
 public class Climber {
-    private TalonSRX ClimberR;
-    private TalonSRX ClimberL;
-    private TalonSRX PullClimberR;
-    private TalonSRX PullClimberL;
+    private TalonSRX climber;
 
 
-    public Climber (int ClimberRID,int ClimberLID, int PullClimberLID, int PullClimberRID) {
-        ClimberR = new TalonSRX(ClimberRID);
-        ClimberL = new TalonSRX(ClimberLID);
-        PullClimberR = new TalonSRX(PullClimberRID);
-        PullClimberL = new TalonSRX(PullClimberLID);
+
+    /**
+     * Sets the id
+     * @param ClimberRID This is the canbus id for the motor
+     */
+    public Climber (int ClimberRID) {
+        climber = new TalonSRX(ClimberRID);
+       
     }
-
+    /**
+     * 
+     * @param speed
+     */
     public void runClimber(double speed){
-        runClimberL(speed);
-        runClimberR(speed);
+        climber.set(ControlMode.PercentOutput, speed);
     }
 
-    // there only needs to be one run climber method / side (it is a one directional control)
-    public void runClimberR(double speed) {
-        ClimberR.set(ControlMode.PercentOutput, speed);
-    }
-
-    public void runClimberL(double speed) {
-        ClimberL.set(ControlMode.PercentOutput, speed);
-    }
-
-    public void runPullClimber(double speed){
-        runPullClimberL(speed);
-        runPullClimberR(speed);
-    }
-
-    public void runPullClimberL(double speed) {
-        PullClimberL.set(ControlMode.PercentOutput, speed);
-    }
-
-    public void runPullClimberR(double speed) {
-        PullClimberR.set(ControlMode.PercentOutput, speed);
-    }
 }
