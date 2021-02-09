eb6111a0d6483aa828480b78bf68610c917d6c89
Nick Farley <nick-1154@hotmail.com>
2021-02-09 16:46:11 -0600

    fix

--- src/main/java/frc/actions/DriveDistance.java
+++ src/main/java/frc/actions/DriveDistance.java
@@ -6,34 +6,37 @@ import frc.robot.Robot;
 
 public class DriveDistance implements Actionable {
 
-    private final double feet;
+    private int distance;
 
     private static final double countsPerFoot = 5215;
 
 
-    public DriveDistance(double feet) {
-        this.feet = feet;
+    public DriveDistance() {
+        
+        this.distance = distance;
     }
 
     @Override
     public void startAction() {
         SmartDashboard.putString("ActionName", "DriveDistance");
+        distance = Robot.drive.getRightDistance() + 1000000;
+        Robot.drive.tankDriveMotionMagic(1000000, 1000000);
     }
 
     @Override
     public void periodic() {
-        Robot.drive.driverFeet(feet);
        // SmartDashboard.putString("DistanceDriven", feet);
     }
 
     @Override
     public void endAction() {
-        Robot.drive.tankDriveVelocity(0, 0);
+        //Robot.drive.tankDriveVelocity(0, 0);
     }
 
     @Override
     public boolean isFinished() {
-        return Robot.drive.leftDriveError() < 500;
+        return false;
+        //return distance <= Robot.drive.getRightDistance();
         //return Robot.drive.rightDriveError() < 500;
     }
 }
