c134240d1ce1a7e3eaa0650db7f7098c66dbe3db
ModernTinTin1 <60020301+ModernTinTin1@users.noreply.github.com>
2021-03-23 18:08:51 -0500

    Seek v2

--- src/main/java/frc/actions/Seek.java
+++ src/main/java/frc/actions/Seek.java
@@ -13,14 +13,17 @@ public class Seek implements Actionable {
     @Override
     public void periodic() {
         if (Robot.drive.getCurrentAngle() < 0) {
-            Robot.drive.tankDriveVelocity(0.3, 0.3);
-            
+            Robot.drive.tankDriveVelocity(0.3, 0.1);
         }
+        else if (Robot.drive.getCurrentAngle() > 0) {
+            Robot.drive.tankDriveVelocity(0.1, 0.3);
+        }
+        
     }
 
     @Override
     public void endAction() {
-        
+        Robot.drive.tankDriveVelocity(0, 0);
     }
 
     @Override
