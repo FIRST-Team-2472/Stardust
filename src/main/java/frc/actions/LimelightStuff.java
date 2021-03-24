ce8f35624c647d93aa387c44ac9430660d116546
ModernTinTin1 <60020301+ModernTinTin1@users.noreply.github.com>
2021-03-24 08:25:04 -0500

    Limelight 1.2

--- src/main/java/frc/actions/LimelightStuff.java
+++ src/main/java/frc/actions/LimelightStuff.java
@@ -1,21 +1,23 @@
 package frc.actions;
 
-import javax.security.sasl.SaslException;
+import javax.swing.Action;
 
+import frc.actions.runners.Actionable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.actions.runners.Actionable;
 import frc.robot.Robot;
 
-public class RangeFinding implements Actionable {
+//WARNING: THIS IS WES JUST MESSING WITH LIMELIGHT CODE. DO NOT TAKE THIS SERIOUSLY UNLESS IT SOMEHOW WORKS.
+public class LimelightStuff implements Actionable {
 
     private double currentDistance = Robot.limelight.distanceIN();
     private double distanceErrorForward = 96-Robot.limelight.distanceIN();
     private double distanceErrorBackward = 120+Robot.limelight.distanceIN();
+    private double KpAim = -0.1;
 
     @Override
     public void startAction() {
-        SmartDashboard.putString("ActionName", "RangeFinding");
-        SmartDashboard.putNumber("DistanceFromFiring", currentDistance);
+        SmartDashboard.putString("ActionName", "limelight stuff");
     }
 
     @Override
@@ -26,19 +28,25 @@ public class RangeFinding implements Actionable {
         else if (distanceErrorBackward > 240 && distanceErrorForward > -24) {
             Robot.drive.tankDriveVelocity(0.3, 0.3);
         }
+        if (Robot.limelight.targetXAngleFromCenter() > 0) {
+            Robot.turret.runTurret(KpAim*Robot.limelight.targetXAngleFromCenter());
+        }
+        else if (Robot.limelight.targetXAngleFromCenter() < 0) {
+            Robot.turret.runTurret(-KpAim*Robot.limelight.targetXAngleFromCenter());
+        }
     }
 
     @Override
     public void endAction() {
         Robot.drive.tankDriveVelocity(0, 0);
+        Robot.turret.runTurret(0);
     }
 
     @Override
     public boolean isFinished() {
-    if (currentDistance < 120 && currentDistance > 96 && Robot.limelight.isTargetSpotted() == true)
+        if (currentDistance < 120 && currentDistance > 96 && Robot.limelight.isTargetSpotted() == true && Robot.limelight.targetXAngleFromCenter() < 0.5)
         return true;
     else 
         return false;
     }
-
 }
